db2a421569de443ba9e4b8a72bd8f62ff866c475
Stefan Nelson-Lindall <bethebunny@gmail.com>
2021-07-12 16:39:27 -0700

    simple parquet vizualization

--- dbv/cli.py
+++ dbv/cli.py
@@ -4,15 +4,19 @@ import click
 from rich.live import Live
 
 from dbv.tui import layout
+from dbv.df import load_df, df_to_rich_table
 
 
 @click.command()
-def main() -> None:
+@click.argument("filename")
+def main(filename) -> None:
     """Startup Database Viewer
 
     TODO add more info to this help message
     """
     with Live(layout, screen=True):
+        df = load_df(filename)
+        layout["main"].update(df_to_rich_table(df, title=filename))
         while True:
             sleep(1)
 
---
+++ dbv/df.py
@@ -0,0 +1,17 @@
+from dask import dataframe as dd
+from rich.table import Table
+
+def load_df(filename):
+    return dd.read_parquet(filename)
+
+
+def df_to_rich_table(df, title=None):
+    table = Table(title=title)
+    table.add_column(" ")
+    for column in df.columns:
+        table.add_column(column)
+
+    for i, row in df.iterrows():
+        table.add_row(*map(str, [i, *row]))
+
+    return table
